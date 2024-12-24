593d203dd9f7d831522e136a1cd7809342f70de8
HadiAlrihani <hadialrihani@gmail.com>
2024-12-24 16:02:13 -0700

    complete rock paper scissors console game added function to play each individual round, as well as a function to play a game of 5 rounds. results of each round/game are outputted to the console.

--- rps.js
+++ rps.js
@@ -1,6 +1,4 @@
 //Javascript file for rock paper scissors console game
-var humanScore = 0;
-var computerScore = 0;
 
 function getComputerChoice()
 {
@@ -22,4 +20,72 @@ function getHumanChoice() {
     let choice = prompt("Rock, Paper, or Scissors?");
     return choice.toLowerCase();
 }
-console.log(getHumanChoice());
+
+function playGame()
+{
+    var humanScore = 0;
+    var computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == "rock") {
+            if (computerChoice == "rock") {
+                console.log("Tie!");
+            }
+            else if (computerChoice == "paper") {
+                console.log("You lose! paper beats rock");
+                computerScore++;
+            }
+            else //computer choice is scissors
+            {
+                console.log("You win! rock beats scissors");
+                humanScore++;
+            }
+        }
+
+        else if (humanChoice == "paper") {
+            if (computerChoice == "paper") {
+                console.log("Tie!");
+            }
+            else if (computerChoice == "scissors") {
+                console.log("You lose! scissors beats paper");
+                computerScore++;
+            }
+            else //computer choice is rock
+            {
+                console.log("You win! paper beats rock");
+                humanScore++;
+            }
+        }
+
+        else if (humanChoice == "scissors") {
+            if (computerChoice == "scissors") {
+                console.log("Tie!");
+            }
+            else if (computerChoice == "rock") {
+                console.log("You lose! rock beats scissors.");
+                computerScore++;
+            }
+            else //computer choice is paper
+            {
+                console.log("You win! scissors beats paper");
+                humanScore++;
+            }
+        }
+    }
+
+    for(let i = 0; i < 5; i++)
+    {   //get choices, and then play a round 5 separate times
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    if (humanScore > computerScore) {
+        console.log("You win the game %d to %d", humanScore, computerScore);
+    }
+    else if (computerScore > humanScore) {
+        console.log("You lose the game %d to %d", computerScore, humanScore);
+    }
+    else {console.log("The game is a draw with a score %d to %d", humanScore, computerScore);}
+}
+
+playGame();
